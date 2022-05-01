224510c96ebe31941e09a90338a7a63a7b372486
gina305 <napier.gina@gmail.com>
2022-05-01 11:50:22 -0400

    Updated movie api

--- src/Movies.js
+++ src/Movies.js
@@ -1,22 +1,30 @@
 import React from "react"; //Import the React Component
 import './App.css' //Link CSS file
 import Card from 'react-bootstrap/Card';
-//Create an app compnent from react's original component. Similar to how classes work
-class Weather extends React.Component {
 
-  //Display the hidden text conditionally
+//Create an app compnent from react's original component. Similar to how classes work
+class Movie extends React.Component {
+
+  //Display movies
+
+
 
   //Return JSX - which allows us to use javascript to render html
   render() {
 
+    let arr = this.props.data;
+   // console.log(arr);
     //
     const map1 = this.props.data.map((x, index)=>
 
-      <Card style={{ width: '33%'}} id="card" key={index} className="card">
+      <Card  id="movieCards" key={index} className="card">
         <Card.Body>
-          <Card.Title>Date: {x.date}</Card.Title>
+          <Card.Title>{x.title}</Card.Title>
           <Card.Text>
-            Description: {x.description}
+           <img src={x.image} alt={x.title}></img>
+           </Card.Text>
+           <Card.Text>
+           Released: {x.release_date}
           </Card.Text>
         </Card.Body>
       </Card>
@@ -26,13 +34,13 @@ class Weather extends React.Component {
     
     return (
       <>
-      <h3>Forecast</h3>
+      <h3>Upcoming Movies</h3>
       {map1}
 
       </>
     )
   }
 }
-export default Weather; //Make the component available for import
+export default Movie; //Make the component available for import
 
 //NOTE: Components are created as js files in the src folder
